ab91e5e5f6072c51947aaa21d269ee3ae575adcc
Luca Aiv <biohazard911@hotmail.it>
2022-04-04 14:10:55 +0200

    Added text renderer

---
+++ inc/text.h
@@ -0,0 +1,24 @@
+#ifndef TEXT_H
+#define TEXT_H
+
+#include "context.h"
+#include "color.h"
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+
+typedef struct render_text{
+    SDL_Surface* text_surface;
+    SDL_Texture* copy_texture;
+    TTF_Font* font;
+    SDL_Color color;
+    int width;
+    int height;
+}render_text_t;
+
+render_text_t* text_new(const char* font, uint16_t font_size, const char* text, color_t color);
+void text_draw(render_text_t* render_text, int size_x, int size_y, int screen_x, int screen_y);
+void text_update(render_text_t* render_text, char* new_text);
+void text_destroy(render_text_t* render_text);
+
+#endif
---
+++ src/text.c
@@ -0,0 +1,62 @@
+#include "text.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern renderer_t *renderer;
+
+render_text_t *text_new(const char *font, uint16_t font_size, const char *text, color_t color)
+{
+    render_text_t *render_text = (render_text_t *)malloc(sizeof(render_text_t));
+    if (!render_text)
+    {
+        printf("Could not allocate enough bytes for struct text!\n");
+        return NULL;
+    }
+    memset(render_text, 0, sizeof(render_text_t));
+
+    render_text->font = TTF_OpenFont(font, font_size);
+
+    SDL_Color text_color = {color.r, color.g, color.b, color.a};
+    render_text->text_surface = TTF_RenderText_Blended(render_text->font, text, text_color);
+    render_text->copy_texture = SDL_CreateTextureFromSurface((SDL_Renderer *)renderer->sdl_renderer, render_text->text_surface);
+
+    render_text->color = text_color;
+
+    SDL_QueryTexture(render_text->copy_texture, NULL, NULL, &render_text->width, &render_text->height);
+
+    return render_text;
+}
+
+void text_draw(render_text_t *render_text, int size_x, int size_y, int screen_x, int screen_y)
+{
+    if (!render_text)
+    {
+        printf("Text pointer is null, please use text_new() predicate, before calling text_draw!!\n");
+        return;
+    }
+
+    SDL_Rect dstrect = {screen_x, screen_y, render_text->width, render_text->height};
+    SDL_RenderCopy((SDL_Renderer *)renderer->sdl_renderer, render_text->copy_texture, NULL, &dstrect);
+}
+
+void text_update(render_text_t *render_text, char *new_text)
+{
+    SDL_FreeSurface(render_text->text_surface);
+    SDL_DestroyTexture(render_text->copy_texture);
+
+    render_text->text_surface = TTF_RenderText_Blended(render_text->font, new_text, render_text->color);
+    render_text->copy_texture = SDL_CreateTextureFromSurface((SDL_Renderer *)renderer->sdl_renderer, render_text->text_surface);
+
+    // also update width and height that might be changed if the new text is wider
+    SDL_QueryTexture(render_text->copy_texture, NULL, NULL, &render_text->width, &render_text->height);
+}
+
+void text_destroy(render_text_t *render_text)
+{
+    SDL_FreeSurface(render_text->text_surface);
+    SDL_DestroyTexture(render_text->copy_texture);
+    TTF_CloseFont(render_text->font);
+    free(render_text);
+}
